3b4b252379871a77f9ac954d504073204a9562d1
Abhijeet Singh <104694989+Abhithecoder-22@users.noreply.github.com>
2023-08-20 22:02:47 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // database module
 var mysql = require('mysql');
 var config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test'
+    host: 'process.env.DB_HOST',
+    user: 'process.env.DB_USERNAME',
+    password: 'process.env.DB_PASSWORD',
+    database: 'process.env.DB_DBNAME'
 };
 
 // init database
