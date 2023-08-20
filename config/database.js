60f2a1666c446dae8366f9235e498f55a1edad57
Abhijeet Singh <104694989+Abhithecoder-22@users.noreply.github.com>
2023-08-20 22:38:46 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // database module
 var mysql = require('mysql');
 var config = {
-    host: 'process.env.DB_HOST',
-    user: 'process.env.DB_USERNAME',
-    password: 'process.env.DB_PASSWORD',
-    database: 'process.env.DB_DBNAME'
+    host: 'bxzhycbdttw1guyq6ewv-mysql.services.clever-cloud.com',
+    user: 'u4cdqbspeona4q8l',
+    password: 'Z8hgGFK2pJ6qjUX2QQkG',
+    database: 'bxzhycbdttw1guyq6ewv'
 };
 
 // init database
